7dcc5f4485ac854e571406d660155dee78e19395
Lit Phansiri <phansiri@uw.edu>
2015-07-27 15:26:21 -0700

    initial push for test web crawler

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 __author__ = 'Phansiri'
 
-var test = "test 2";
+
+import requests
+from bs4 import BeautifulSoup
+
+def trade_spider(max_pages):
+    page = 1
+    while page < max_pages:
+        url = 'http://students.washington.edu/phansiri/'\
+              # + str(page)
+        source_code = requests.get(url)
+        plain_text = source_code.text
